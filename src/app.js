dd89988307e068e2f1c80ab500fd3472a23b4958
Maxim-LD <ayinde779@gmail.com>
2025-01-22 21:20:24 +0100

    version 2 added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express"
-import paymentRouter from "./route/paymentRouter.js"
+import v1Router from ".//route/v1/paymentRoutes.js"
+import v2Router from "./route/v2/paymentRoutes.js"
 
 
 const app = express()
 
 app.use(express.json())
 
-app.use("/api", paymentRouter)
+app.use("/api", v1Router)
+app.use("/api", v2Router)
 
 app.use((req, res) => {
   res.status(404).json({ message: "This endpoints does not exist" })
